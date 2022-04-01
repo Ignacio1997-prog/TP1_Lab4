71b0dea9d9a48dbbc6a9437d26de786b1ec6ee85
Ignacio Cardozo <57219363+Cardo4@users.noreply.github.com>
2022-03-31 22:42:07 -0300

    Update Principal.java

--- src/Ejercicio1/Principal.java
+++ src/Ejercicio1/Principal.java
@@ -4,13 +4,25 @@ public class Principal {
 
 	public static void main(String[] args) {
 		
-		Empleado emp1 = new Empleado(1,"Manuel");
-		Empleado emp2 = new Empleado("Sofia",3);
-		Empleado emp3 = new Empleado();
+		Empleado emp1 = new Empleado(30,"Manuel");
+		Empleado emp2 = new Empleado("Sofia",28);
+		Empleado emp3 = new Empleado(22,"Tomas");
 		
-		System.out.println(emp1.toStringTodo());
-		System.out.println(emp2.toStringTodo());
-		System.out.println(emp3.toStringTodo());
+		Empleado emp4 = new Empleado();
+		emp4.setEdad(25);
+		emp4.setNombre("Jose");
+		Empleado emp5 = new Empleado();
+		emp5.setNombre("Camila");
+		emp5.setEdad(33);
+		
+		System.out.println(emp1.toString());
+		System.out.println(emp2.toString());
+		System.out.println(emp3.toString());
+		System.out.println(emp4.toString());
+		System.out.println(emp5.toString());
+		
+		System.out.println(emp1.devuelveProximoID());
+		
 		return;
 	}
 
